9c683b307860a09c7513430af99dcc8c5efa1483
irineonyx <85147915+irineonyx@users.noreply.github.com>
2025-06-30 13:28:54 +0700

    Fix minor issue for Vercel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import User from './components/User';
-import Repository from './components/Repository';
 
 const App: React.FC = () => {
-  const [selectedUser, setSelectedUser] = useState<string | null>(null);
-
   return (
     <div className="min-h-screen bg-white text-black p-4 max-w-md mx-auto">
       <User />
